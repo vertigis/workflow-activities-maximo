d047087bccb171be86942de0dd1c59c494715489
Ryan Cooney <rcooney@latitudegeo.com>
2021-03-03 10:26:57 -0800

    fix: Improve extracting of IDs from URLs

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,7 @@
 export function getIdFromIdOrUrl(idOrUrl: string): string {
-    // Extract the ID from a URL
-    return idOrUrl.substring(idOrUrl.lastIndexOf("/") + 1);
+    if (/^https?:\/\//i.test(idOrUrl)) {
+        // Extract the ID from a URL
+        return idOrUrl.substring(idOrUrl.lastIndexOf("/") + 1);
+    }
+    return idOrUrl;
 }
